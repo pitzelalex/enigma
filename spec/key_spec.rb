6ec159f8405bf46ce5d1d04f9c1d362551b20fc3
Alex Pitzel <pitzelalex@gmail.com>
2022-11-11 12:40:05 -0500

    test: Create Key #a_key

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -6,11 +6,26 @@ require './lib/key'
 describe Key do
   describe '#initialize' do
     it 'instantiates correctly' do
-      key = Key.new
+      key1 = Key.new
+      key2 = Key.new(base: '12345')
 
-      expect(key).to be_a Key
-      expect(key.base.length).to be 5
-      expect(key.base).to be_a String
+      expect(key1).to be_a Key
+      expect(key1.base.length).to be 5
+      expect(key1.base).to be_a String
+      expect(key2).to be_a Key
+      expect(key2.base).to eq('12345')
+    end
+  end
+
+  describe 'key methods' do
+    let(:key1) { Key.new(base: 12345) }
+    let(:key2) { Key.new(base: 54321) }
+
+    describe '#a_key' do
+      it 'returns the first 2 characters of the base' do
+        expect(key1.a_key).to eq(12)
+        expect(key2.a_key).to eq(54)
+      end
     end
   end
 end
