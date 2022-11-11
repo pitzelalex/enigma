d069bcdfc6037bee3f7fb15d4eb3e6f87124e4e0
Alex Pitzel <pitzelalex@gmail.com>
2022-11-11 15:21:03 -0500

    feat: Create Offset #convert_date

--- lib/offset.rb
+++ lib/offset.rb
@@ -9,4 +9,11 @@ class Offset
   def initialize(date: Date.today)
     @date = date
   end
+
+  def convert_date
+    d = @date.strftime('%d')
+    m = @date.strftime('%m')
+    y = @date.strftime('%y')
+    @_converted_date ||= m + d + y
+  end
 end
