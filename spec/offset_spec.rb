906419775ed1e3d80ebed2b1630a1be9fe23652e
Alex Pitzel <pitzelalex@gmail.com>
2022-11-11 13:35:30 -0500

    test: Update Offset #initialize test

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -8,13 +8,12 @@ describe Offset do
   describe '#initialize' do
     it 'instantiates correctly' do
       offset1 = Offset.new
-      # offset2 = Offset.new(date: )
+      offset2 = Offset.new(date: Date.parse('May 29, 1993'))
 
       expect(offset1).to be_a Offset
       expect(offset1.date).to be_a Date
       expect(offset1.date).to eq(Date.today)
-      # expect(key2).to be_a Offset
-      # expect(key2.base).to eq('12345')
+      expect(offset2.date).to eq(Date.parse('May 29, 1993'))
     end
   end
 end
