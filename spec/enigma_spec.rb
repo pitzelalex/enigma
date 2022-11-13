a3b8fc01126e1d52ed46d97a1211e0155c21a99c
Alex Pitzel <pitzelalex@gmail.com>
2022-11-13 13:48:17 -0500

    test: Create Enigma #encrypt

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,4 +11,18 @@ describe Enigma do
       expect(enigma).to be_a Enigma
     end
   end
+
+  describe '#encrypt' do
+    it 'returns a hash with encrypted message / associated key' do
+      enigma = Enigma.new
+
+      expect(enigma.encrypt('hello world', '02715', '040895')).to eq(
+        {
+          encryption: 'keder ohulw',
+          key: '02715',
+          date: '040895'
+        }
+      )
+    end
+  end
 end
