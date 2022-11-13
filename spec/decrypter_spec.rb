3f7465227a7ec8937fe5cea282665a858e367664
Alex Pitzel <pitzelalex@gmail.com>
2022-11-13 12:54:38 -0500

    test: Create Decrypter #decrypt test

--- spec/decrypter_spec.rb
+++ spec/decrypter_spec.rb
@@ -16,4 +16,14 @@ describe Decrypter do
       expect(crypt.input).to eq('aawlh,wgokho!')
     end
   end
+
+  describe '#decrypt' do
+    it 'changes the characters of a string based on the key and offset' do
+      key = Key.new(base: 12345)
+      offset = Offset.new(date: Date.parse('May 29, 1993'))
+      crypt = Decrypter.new(key: key, offset: offset, input: 'aawlh,wgokho!')
+
+      expect(crypt.decrypt).to eq('hello, world!')
+    end
+  end
 end
