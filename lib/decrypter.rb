2544edc2b999444e445c6be8658a508bee2a3c48
Alex Pitzel <pitzelalex@gmail.com>
2022-11-13 12:58:49 -0500

    feat: Create Decrypter #decrypt

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -14,4 +14,25 @@ class Decrypter
     @offset = offset
     @input = input
   end
+
+  def decrypt
+    alphabet = ('a'..'z').to_a << ' '
+    input_arr = @input.split('')
+    a_encryption = @key.a_key.to_i + @offset.a_offset.to_i
+    b_encryption = @key.b_key.to_i + @offset.b_offset.to_i
+    c_encryption = @key.c_key.to_i + @offset.c_offset.to_i
+    d_encryption = @key.d_key.to_i + @offset.d_offset.to_i
+    crypt_arr = [a_encryption, b_encryption, c_encryption, d_encryption]
+    decrypted = input_arr.map do |character|
+      if alphabet.include?(character)
+        alphabet.rotate! until alphabet[0] == character
+        alphabet.rotate!(-crypt_arr[0])
+        crypt_arr.rotate!
+        alphabet[0]
+      else
+        character
+      end
+    end
+    decrypted.join.to_s
+  end
 end
