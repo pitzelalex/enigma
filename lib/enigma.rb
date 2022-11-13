6688b08cdd3c4ec14b3fefd60aa4eaa9caa2c035
Alex Pitzel <pitzelalex@gmail.com>
2022-11-13 14:02:44 -0500

    feat: Create Enigma #encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'encrypter'
 
+# this is the Enigma class
 class Enigma
-  def initialize
+  def initialize; end
+
+  def encrypt(input, key, date)
+    encrypter = Encrypter.new(input: input, key: Key.new(base: key), offset: Offset.new(date: date))
+    {
+      encryption: encrypter.encrypt,
+      key: encrypter.key.base,
+      date: encrypter.offset.convert_date
+    }
   end
 end
