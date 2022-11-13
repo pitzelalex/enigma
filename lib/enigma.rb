0ac641fab38655a401d745948bb738aebe93b5d6
Alex Pitzel <pitzelalex@gmail.com>
2022-11-13 14:30:08 -0500

    feat: Create Enigma #decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
 require_relative 'encrypter'
+require_relative 'decrypter'
 
 # this is the Enigma class
 class Enigma
   def initialize; end
 
-  def encrypt(input, key, date)
+  def encrypt(input, key = rand.to_s[2..6], date = Date.today)
     encrypter = Encrypter.new(input: input, key: Key.new(base: key), offset: Offset.new(date: date))
     {
       encryption: encrypter.encrypt,
       key: encrypter.key.base,
       date: encrypter.offset.convert_date
+    }
+  end
+
+  def decrypt(input, key, date = Date.today)
+    decrypter = Decrypter.new(input: input, key: Key.new(base: key), offset: Offset.new(date: date))
+    {
+      decryption: decrypter.decrypt,
+      key: decrypter.key.base,
+      date: decrypter.offset.convert_date
     }
   end
 end
