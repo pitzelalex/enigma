24d06ad136efb84260d1441c52d8409f360a4453
Alex Pitzel <pitzelalex@gmail.com>
2022-11-14 15:25:05 -0500

    test: Create Cypherable b and c_cypher methods

--- spec/cypherable_spec.rb
+++ spec/cypherable_spec.rb
@@ -18,6 +18,7 @@ describe Cypherable do
       expect(encrypter.a_cypher).to eq(20)
     end
   end
+
   describe '#b_cypher' do
     it 'combines the b_key and b_offset' do
       key = double('key')
@@ -30,12 +31,30 @@ describe Cypherable do
       expect(encrypter.b_cypher).to eq(23)
     end
   end
+
+  describe '#c_cypher' do
+    it 'combines the c_key and c_offset' do
+      key = double('key')
+      offset = double('offset')
+      allow(key).to receive(:c_key).and_return('34')
+      allow(offset).to receive(:c_offset).and_return('4')
+
+      encrypter = Encrypter.new(input: 'test', key: key, offset: offset)
+
+      expect(encrypter.c_cypher).to eq(38)
+    end
+  end
+
+  describe '#d_cypher' do
+    it 'combines the d_key and d_offset' do
+      key = double('key')
+      offset = double('offset')
+      allow(key).to receive(:d_key).and_return('45')
+      allow(offset).to receive(:d_offset).and_return('9')
+
+      encrypter = Encrypter.new(input: 'test', key: key, offset: offset)
+
+      expect(encrypter.d_cypher).to eq(54)
+    end
+  end
 end
-
-# allow(key).to receive(:c_key).and_return('34')
-# allow(offset).to receive(:c_offset).and_return('4')
-# expect(encrypter.c_cypher).to eq(48)
-
-# allow(key).to receive(:d_key).and_return('45')
-# allow(offset).to receive(:d_offset).and_return('9')
-# expect(encrypter.d_cypher).to eq(54)
